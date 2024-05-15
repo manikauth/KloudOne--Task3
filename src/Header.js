501a1d9c90fb7e40007f62e547c29a53b125a3e4
manikauth <manikandan@authnull.com>
2024-05-15 15:06:05 +0530

    AV-964

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,16 @@ function Header() {
   return (
     <div>
       <h2 style={{ marginLeft: "30px" }}>Tenant</h2>
+
       <Button className="tenant-btn" type="primary">
         Create Tenant
       </Button>
-      <Button className="search-btn">Search</Button>
+
+      <div>
+        
+        <h3>Search</h3>
+        <Button className="search-btn">Search</Button>
+      </div>
     </div>
   );
 }
