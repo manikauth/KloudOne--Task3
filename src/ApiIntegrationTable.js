7d90c84f433812f57200f1a92f92e7163ae97cee
manikauth <manikandan@authnull.com>
2024-05-15 14:54:46 +0530

    AV-964

--- src/ApiIntegrationTable.js
+++ src/ApiIntegrationTable.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'antd';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Table, Tag } from "antd";
+import axios from "axios";
 
 const ApiIntegrationTable = () => {
   const [loading, setLoading] = useState(true);
@@ -10,17 +10,20 @@ const ApiIntegrationTable = () => {
     // Function to fetch data from API
     const fetchData = async () => {
       try {
-        const response = await axios.post('https://prod.tenants.authnull.com/getTenantDetail', {
-          email: 'muthu@authnull.com',
-          tenantId: 3,
-          orgId: 89
-        });
+        const response = await axios.post(
+          "https://prod.tenants.authnull.com/getTenantDetail",
+          {
+            email: "muthu@authnull.com",
+            tenantId: 3,
+            orgId: 89,
+          }
+        );
 
         const data = response.data;
         setDataSource([data.data]); // Assuming response is an object, convert it to an array if response is an array
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
@@ -30,34 +33,47 @@ const ApiIntegrationTable = () => {
 
   const columns = [
     {
-      title: 'Organization Name',
-      dataIndex: 'organizationName',
-      key: 'organizationName'
+      title: "Organization Id",
+      dataIndex: "orgId",
+      key: "orgId",
     },
     {
-      title: 'Tenant Name',
-      dataIndex: 'tenantname',
-      key: 'tenantname'
+      title: "Organization Name",
+      dataIndex: "organizationName",
+      key: "organizationName",
     },
     {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email'
+      title: "Tenant Id",
+      dataIndex: "tenantid",
+      key: "    tenantid",
     },
     {
-      title: 'URL',
-      dataIndex: 'url',
-      key: 'url'
+      title: "Tenant Name",
+      dataIndex: "tenantname",
+      key: "tenantname",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      render: (email) => {
+        return <Tag color="green">{email}</Tag>;
+      },
+    },
+    {
+      title: "URL",
+      dataIndex: "url",
+      key: "url",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Authentication Method",
+      dataIndex: "authenticationmethod",
+      key: "authenticationmethod",
     },
   ];
 
-  return (
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      loading={loading}
-    />
-  );
+  return <Table dataSource={dataSource} columns={columns} loading={loading} />;
 };
 
 export default ApiIntegrationTable;
